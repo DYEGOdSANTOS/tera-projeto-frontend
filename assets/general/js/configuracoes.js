08843a6bf98f7e6fd03957c8b08bdff56cd5b192
BuildTools <unconfigured@null.spigotmc.org>
2022-08-15 20:29:31 -0300

    15-08-2022 20:29

--- assets/general/js/configuracoes.js
+++ assets/general/js/configuracoes.js
@@ -1,25 +1,31 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-}
-
 $('.form-control').addClass('form-control-sm')
 
-$('.btn-view').click(function(){
-    let type, html;
-    if ($(this).attr('data') === 'text'){
-        type = 'password';
-        html = '<i class="bi bi-eye-fill">';
-    }else{
-        type = 'text';
-        html = '<i class="bi bi-eye-slash-fill">';
-    }
-    let input = $($(this).attr('data-target'));
-    input.prop('type', type);
-    $(this).attr('data', type);
-    $(this).html(html);
-});
+$(function() {
+    $('.btn-view').click(function() {
+        let type, html;
+        if ($(this).attr('data') === 'text') {
+            type = 'password';
+            html = '<i class="bi bi-eye-fill">';
+        } else {
+            type = 'text';
+            html = '<i class="bi bi-eye-slash-fill">';
+        }
+        let input = $($(this).attr('data-target'));
+        input.prop('type', type);
+        $(this).attr('data', type);
+        $(this).html(html);
+    });
+
+    $('.btn-confirm').click(function() {
+        $('#modal-configuracoes').modal('show');
+        let element = $(this);
+        $('#element-name').html(element.attr('data'));
+        $('#safe-name').html(element.attr('data'));
+        $('#element-content').html(`Confirma alteração de <b>${element.attr('data')}</b>?`);
+    });
+
+    $('#btn-save').click(function() {
+        $('#modal-configuracoes').modal('hide');
+        $('#modal-safe').modal('show');
+    });
+})
